b27cd2f8b4a738a20639760d2938cf093f8a29e5
vanya pavlova <vanya.pavlova@primeholding.com>
2019-03-06 13:43:43 +0200

    Setting up Mongoose

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+const mongoose = require('mongoose')
+const db = "mongodb://localhost:27017/dbEvents"
+
+mongoose.connect(db, { useNewUrlParser: true}, err => {
+  if(err) {
+    console.error('Error' + err);
+  } else {
+    console.log('Connected to MongoDB')
+  }
+})
 router.get('/', (req,res) => {
   res.send('API router');
 })
